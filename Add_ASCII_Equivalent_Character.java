b4b01578ce540b1b04c1e778662b2ff02e9a3c46
immkrish <92216704+immkrish@users.noreply.github.com>
2026-01-23 18:13:59 +0530

    Add files via upload

---
+++ Add_ASCII_Equivalent_Character.java
@@ -0,0 +1,14 @@
+package assignment_Datatypes;
+
+public class Add_ASCII_Equivalent_Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char value = 'd';
+		int addvalues = (int)value+3;
+		System.out.println("After adding 3 to d Charcter ASCII Value:"+addvalues);
+		System.out.println("Equivalent ASCII value for added number is:"+(char)addvalues);
+		
+	}
+
+}
---
+++ Add_Numbers.java
@@ -0,0 +1,15 @@
+package assignment_Datatypes;
+
+public class Add_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100;
+		int b = 200;
+		int c = 3400;
+		System.out.println("Your Total Amount is:"+(a+b+c));
+
+	}
+
+}
