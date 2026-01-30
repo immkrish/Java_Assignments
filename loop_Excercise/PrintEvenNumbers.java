95739dc86200d393b3f8a12d35a1cfd40c6af1de
immkrish <92216704+immkrish@users.noreply.github.com>
2026-01-30 21:35:19 +0530

    Add files via upload

---
+++ loop_Excercise/PrintEvenNumbers.java
@@ -0,0 +1,15 @@
+package loop_Excercise;
+
+public class PrintEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =2;
+		while(i<=100)
+		{
+		System.out.println(i);
+		i=i+2;
+		}
+	}
+
+}
---
+++ loop_Excercise/Print_Ten_Numbers.java
@@ -0,0 +1,16 @@
+package loop_Excercise;
+
+public class Print_Ten_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		int i=1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+	}
+
+}
